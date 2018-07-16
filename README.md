40ed3c5e6a396f934dbac6f08514188f2bd04bad
catoxliu <catoxliu@tencent.com>
2018-07-16 20:22:33 +0800

    simple tape demo works now, still has a bug with concave obj

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# unity_demos
-simple unity3d demos
+# Simple Tape Demo
+
+Hold the "space" button down will keep the tape (white line) grows, and release the button will wrap the object. There still a bug when wrap a concave object.
+
+![Wrap Tape](simple_tape/doc/images/wrap_tape_20180716.gif)
--- simple_tape/Assets/Scripts/TapeBehaviour.cs
+++ simple_tape/Assets/Scripts/TapeBehaviour.cs
@@ -4,37 +4,121 @@ using UnityEngine;
 
 public class TapeBehaviour : MonoBehaviour {
 
-    public float mTapeIncrement = 0.003f;
+    public float mfTapeIncrement = 0.003f;
 
     private Mesh mTapeMesh;
+    private float mfTapeWidth = 0.01f;
+    private float mfDefaultTapeHeight = 0.1f;
+    private float mfTapeRotateSpeed = -1;
+    private GameObject mTape;
 
 	// Use this for initialization
 	void Start () {
-        var mf = GetComponent<MeshFilter>();
-        mTapeMesh = mf.mesh;
-        if (mTapeMesh == null)
-        {
-            Debug.LogError("Could not find tape!");
-        }
-	}
+        mTape = transform.GetChild(0).gameObject;
+        mTapeMesh = new Mesh();
+        mTapeMesh.vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(-mfTapeWidth, 0, 0), new Vector3(0, mfDefaultTapeHeight, 0), new Vector3(-mfTapeWidth, mfDefaultTapeHeight, 0), new Vector3(-mfTapeWidth, 0, 0) };
+        mTapeMesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1), new Vector2(1, 0), new Vector2(0,0) };
+        mTapeMesh.triangles = new int[] { 0, 1, 2, 1, 3, 2, 0, 4, 1 };
+        MeshFilter mf = mTape.AddComponent<MeshFilter>();
+        MeshRenderer mr = mTape.AddComponent<MeshRenderer>();
+        mf.sharedMesh = mTapeMesh;
+
+        Material defaultTapeMat = Resources.Load("Materials/DefaultTapeMaterial") as Material;
+        mr.material = defaultTapeMat;
+
+        //transform.position = new Vector3(-mfTapeWidth, 0, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.Space))
         {
             Vector3[] vers = mTapeMesh.vertices;
-            vers[1].y = vers[1].y + mTapeIncrement;
-            vers[3].y = vers[3].y + mTapeIncrement;
+            vers[2].y = vers[2].y + mfTapeIncrement;
+            vers[3].y = vers[3].y + mfTapeIncrement;
             mTapeMesh.vertices = vers;
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            TapeWrap();
+            StartCoroutine(TapeWrap());
         }
-	}
+    }
 
-    void TapeWrap()
+    IEnumerator TapeWrap()
     {
+        var pg = GameObject.FindObjectOfType<PolygonGenerator>();
+        var startPoint = Vector2.zero;
+        var tapeVertices = mTapeMesh.vertices;
+        var tapeUVs = mTapeMesh.uv;
+        var tapeTriangles = mTapeMesh.triangles;
+        for (int i = 0; i <= pg.mPolygonVertices.Length; i++)
+        {
+            var vertice = i == pg.mPolygonVertices.Length ? Vector2.zero : pg.mPolygonVertices[i];
 
+            var edge = vertice - startPoint;
+            var length = edge.magnitude;
+
+            var tapeLength = (tapeVertices[2] - tapeVertices[0]).magnitude;
+            bool bBreak = tapeLength <= length;
+            var wrapVertice = Vector3.MoveTowards(tapeVertices[0], tapeVertices[2], bBreak ? tapeLength : length);
+
+            if (wrapVertice.x == vertice.x && wrapVertice.y == vertice.y)
+            {
+                //break tape mesh
+                BreakMesh(wrapVertice, ref tapeVertices);
+            }
+            else
+            {
+                //rotate the tape
+                var angle = Vector3.SignedAngle(wrapVertice - tapeVertices[0], edge, Vector3.back);
+                Debug.Log(angle);
+                var pivot = tapeVertices[0];
+                while (angle > 1)
+                {
+                    tapeVertices[1] = RotatePointAroundPivod(tapeVertices[1], pivot, mfTapeRotateSpeed);
+                    tapeVertices[2] = RotatePointAroundPivod(tapeVertices[2], pivot, mfTapeRotateSpeed);
+                    tapeVertices[3] = RotatePointAroundPivod(tapeVertices[3], pivot, mfTapeRotateSpeed);
+                    mTapeMesh.vertices = tapeVertices;
+                    angle--;
+                    yield return null;
+                }
+                tapeVertices[1] = RotatePointAroundPivod(tapeVertices[1], pivot, -angle);
+                tapeVertices[2] = RotatePointAroundPivod(tapeVertices[2], pivot, -angle);
+                tapeVertices[3] = RotatePointAroundPivod(tapeVertices[3], pivot, -angle);
+                mTapeMesh.vertices = tapeVertices;
+                yield return null;
+                //break tape mesh
+                if (bBreak) yield break;
+                BreakMesh(vertice, ref tapeVertices);
+            }
+
+            startPoint = vertice;
+        }
+    }
+
+    void BreakMesh(Vector3 wrapVertice, ref Vector3[] tapeVertices)
+    {
+        var wrapMoveDir = wrapVertice - tapeVertices[0];
+
+        GameObject stackTape = Instantiate(mTape, transform);
+        Mesh stackTapeMesh = stackTape.GetComponent<MeshFilter>().mesh;
+        var stackMeshVers = stackTapeMesh.vertices;
+        stackMeshVers[2] = wrapVertice;
+        stackMeshVers[3] = tapeVertices[1] + wrapMoveDir;
+        stackTapeMesh.vertices = stackMeshVers;
+
+        tapeVertices[0] = wrapVertice;
+        tapeVertices[1] = tapeVertices[1] + wrapMoveDir;
+        tapeVertices[4] = tapeVertices[1];
+
+        mTapeMesh.vertices = tapeVertices;
+    }
+
+    Vector3 RotatePointAroundPivod(Vector3 p, Vector3 pivot, float angle)
+    {
+        var dir = p - pivot;
+        dir = Quaternion.Euler(0, 0, angle) * dir;
+        p = dir + pivot;
+        return p;
     }
 }
